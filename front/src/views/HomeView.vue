6a679d5f871947430edd9d999502bd200da4b11e
wangx <36002218@qq.com>
2023-05-28 22:31:26 +0800

    代码调整

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <RouterView />
 </template>
