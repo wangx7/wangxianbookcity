6fa2c8ae4e324445c7bf2f958f635456650a9e35
wangx <36002218@qq.com>
2023-05-29 16:05:34 +0800

    测试husky

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/about',，
       name: 'about',
       component: () => import('../views/AboutView.vue')
     }
