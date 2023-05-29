67718540bc035ad9278c22d68c4e54412bb639b8
wangx <36002218@qq.com>
2023-05-29 17:17:59 +0800

    1

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',，
+      path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
     }
