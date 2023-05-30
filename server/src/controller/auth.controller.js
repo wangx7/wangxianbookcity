ba2cbb3d98c06c5e039bbdf1cc93bc0af81f13b5
wangx <36002218@qq.com>
2023-05-30 11:02:31 +0800

    修改登录后返回的信息

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -2,19 +2,18 @@ const jwt = require('jsonwebtoken');
 const { PRIVATE_KEY } = require('../app/config');
 
 class AuthController {
-  async login(ctx, next) {
-    const { id, name } = ctx.user;
-    const token = jwt.sign({ id, name }, PRIVATE_KEY, {
-      expiresIn: 60 * 60 * 24,
-      algorithm: 'RS256'
-    });
+    async login(ctx, next) {
+        const token = jwt.sign(ctx.user, PRIVATE_KEY, {
+            expiresIn: 60 * 60 * 24,
+            algorithm: 'RS256'
+        });
 
-    ctx.body = { id, name, token }
-  }
+        ctx.body = { ...ctx.user, token };
+    }
 
-  async success(ctx, next) {
-    ctx.body = "授权成功~";
-  }
+    async success(ctx, next) {
+        ctx.body = '授权成功~';
+    }
 }
 
 module.exports = new AuthController();
